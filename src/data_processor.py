1bb2855660aa57cf99c9eb0f56e041736f42a095
shuailiz <shuailirpi@gmail.com>
2019-03-24 03:15:53 -0500

    Add the data processing after copying from the template

---
+++ src/data_processor.py
@@ -0,0 +1,161 @@
+#!/bin/usr/python
+
+import os
+import cv2
+import glob
+from sklearn.utils import shuffle
+import numpy
+
+class DataProcesser(object):
+
+    def __init__(self, class_names, image_size):
+        self.class_names = class_names
+        self.image_size = image_size
+        self.total_num_data_points = 0
+        self.data_loaded = False
+        self.images = []
+        self.labels = []
+        self.cls = []
+        self.image_names = []
+        self.training_data = []
+        self.validation_data = []
+
+        self.training_data_start_ind = 0
+        self.training_data_end_ind = 0
+        self.valid_data_start_ind = 0
+        self.valid_data_start_ind = 0
+        self.training_ind_in_epoch = 0
+        self.valid_ind_in_epoch = 0
+
+    def get_training_data_size(self):
+        ''' Get the total number of points in the training data set
+        '''
+        return self.training_data_end_ind - self.training_data_start_ind + 1
+
+    def read_data_from_folder(self, folder_path):
+        ''' Read data from a folder
+        param:
+        folder_path (str): path to the folder
+        return:
+        input_data: the data read from the folder
+        '''
+        for class_name in self.class_names:
+            data_path = os.path.join(folder, class_name, '*g')
+            data_files = glob.glob(data_path)
+            for data_file in data_files:
+                image = cv2.imread(data_file)
+                # resize the image to be more reasonable
+                image = cv2.resize(image, (self.image_size, self.image_size), 0, 0, cv2.INTER_LINEAR)
+                # cast pixel value to be float
+                image = image.astype(np.float32)
+                # convert the pixle values to be between 0 and 1
+                image = np.multiply(image, 1.0/255.0)
+                label = np.zeros(len(self.class_names))
+                label(class_names.index(class_name)) = 1.0
+                cl = class_name
+                self.images.append(image)
+                self.labels.append(label)
+                self.cls.append(cl)
+                self.image_names(os.path.basename(data_file))
+
+        self.images, self.labels, self.cls, self.image_names = shuffle(self.images, self.labels, self.cls, self.image_names)
+        self.total_num_data_points = len(self.cls)
+        self.data_loaded = True
+
+    def read_data_from_hdf5(self, hdf5_file):
+        ''' Read data from a hdf5 file
+        param:
+        hdf5_file: The hdf5 file handle
+        return:
+        input_data: The data read from the hdf5 file
+        '''
+        pass
+
+    def read_data_from_file(self, file_handle, data_type):
+        ''' Read data from a file
+        param:
+        file_handle: a file reader handle
+        data_type (enum): the type of the data to read
+        return:
+        input_data: the data read from the file handle
+        '''
+        pass
+
+    def save_data_to_hdf5(self, data, hadf5_file):
+        ''' Save data to a hdf5 file
+        param:
+        data: The data to be saved
+        hdf5_file: The file handle to the hdf5 file
+        '''
+        pass
+
+    def validate_data_point(self, data_point):
+        ''' Validate a data point 
+        param:
+        data_point: the data point to validate
+        '''
+        if data_point.shape[0] != self.image_size:
+            print("expected first dim %d, given dim %d".format(self.image_size, data_point.shape[0]))
+            return False
+
+        if data_point.shape[1] != self.image_size:
+            print("expected second dim %d, given dim %d".format(self.image_size, data_point.shape[1]))
+            return False
+
+        if np.isnan(data_point).any():
+            print("None value found in the data point array")
+            return False
+
+        return True
+
+    def divide_training_valid_data(self, training_size):
+        ''' divide the data into training and validation for tensor flow network
+        param:
+        training_size (double): The the size of the training set in the whole data set
+        return:
+        training_data: The data for training
+        '''
+        training_data_num = int(training_size * self.total_num_data_points)
+        self.training_data_start_ind = 0
+        self.training_data_end_ind = training_data_num - 1
+        self.valid_data_start_ind = training_data_num
+        self.valid_data_end_ind = self.total_num_data_points - 1
+        self.training_ind_in_epoch = self.training_data_start_ind
+        self.valid_ind_in_epoch = self.valid_data_start_ind
+
+    def next_training_batch(self, batch_size):
+        ''' Generate the next batch of data for training
+        param:
+        batch_size: How large the batch will be
+        return:
+        training_batch: The batch of training data
+        '''
+        start = self.training_ind_in_epoch
+        end = self.training_ind_in_epoch + batch_size - 1
+
+        if start >= self.training_data_end_ind:
+            start = self.training_data_start_ind
+            end = self.training_data_start_ind + batch_size - 1
+        elif end > self.training_data_end_ind:
+            end = self.training_data_end_ind
+
+        self.training_ind_in_epoch = end + 1
+        return self.images[start:end], self.labels[start:end]
+
+    def next_validate_batch(self, batch_size):
+        ''' Generate the next batch of data for validation
+        param:
+        batch_size: How large the batch will be
+        return:
+        validation_batch: The batch of validation data
+        '''
+        start = self.valid_ind_in_epoch
+        end = self.valid_ind_in_epoch + batch_size - 1
+
+        if start >= self.valid_data_end_ind:
+            start = self.valid_data_start_ind
+            end = self.valid_data_start_ind + batch_size - 1
+        elif end > self.valid_data_end_ind:
+            end = self.valid_data_end_ind
+
+        return self.images[start:end], self.labels[start:end]
---
+++ src/train.py
@@ -0,0 +1,91 @@
+import tf
+import os
+
+from data_processor import DataProcesser
+from graph_constructor import CNNGraph
+
+class TrainCNN(object):
+    def __init__(self, input_x, input_y, input_channel,
+                 num_classes, conv_filter_size, conv_nums_filters, fc_layer_size):
+        self.model_path = os.path.join(os.getcwd(), '../models/')
+        self.batch_size = 50
+
+        self.input_x = input_x
+        self.input_y = input_y
+        self.input_channel = input_channel
+        self.num_classes = num_classes
+        self.conv_filter_size = conv_filter_size
+        self.conv_nums_filters = conv_nums_filters
+        self.fc_layer_size = fc_layer_size
+        self.optimizer = None
+        self.cost = None
+        self.cross_entropy = None
+        self.data_process = DataProcesser()
+        self.data_process.divide_training_valid_data(0.85)
+        self.graph_constructor = CNNgraph()
+
+    def initialize_graph(self):
+        self.graph_constructor.create_graph(self.input_x, self.input_y, self.input_channel, self.num_classes,
+                                            self.conv_filter_size, self.nums_filters, self.fc_layer_size)
+        self.cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.graph_constructor.last_layer,
+                                                                     labels=self.graph_constructor.y_true)
+        self.cost = tf.reduce_mean(self.cross_entropy)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+        self.correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+
+    def read_total_iter(self):
+        tt_iter_file = os.path.join(self.model_dir, 'total_iteration.txt')
+        with open(tt_iter_file, 'r') as f:
+            try:
+                self.total_iter = int(f.read())
+            except:
+                print('Error reading the total number of iterations. Assigning zero')
+                self.total_iter = 0
+
+    def show_progress(self, epoch, feed_dict_train, feed_dict_validate, val_loss, session):
+        acc = session.run(self.accuracy, feed_dict=feed_dict_train)
+        val_acc = session.run(self.accuracy, feed_dict=feed_dict_validate)
+        msg = "Training Epoch {0} --- Training Accuracy: {1:>6.1%}, Validation Accuracy: {2:>6.1%},  Validation Loss: {3:.3f}"
+        print(msg.format(epoch + 1, acc, val_acc, val_loss))
+                
+    def train(self, num_iterations):
+        ''' Train the network for certain number of iterations
+        param:
+        num_iterations: The number of iterations to train
+        '''
+        # TODO:: NEED TO LOAD AND SAVE THE TOTAL ITER
+        total_iter_file = os.path.join(self.model_path, '/total_iter.txt')
+        total_num_iter = 0
+        with tf.Session() as session:
+            if os.path.isfile(self.model_path + '.meta'):
+                # Model has been trained. Restore
+                saver = tf.import_meta_graph(self.model_path + ".meta")
+                saver.restore(session, tf.train.latest_checkpoint(self.model_path))
+                with open(total_iter_file, 'r') as total_iter_file:
+                    total_num_iter = int(total_iter_file.read())
+            else:
+                # Otherwise create the graph
+                self.initialize_graph()
+                self.read_total_iter()
+                saver = tf.train.Saver()
+
+            session.run(tf.global_variables_initializer())
+            for step in range(total_num_iter, total_num_iter + num_iterations):
+                x_batch_train, y_batch_train = self.data_process.next_training_batch(self.batch_size)
+                x_batch_valid, y_batch_valid = self.data_process.next_validate_batch(self.batch_size)
+                feed_dict_train = {x: x_batch_train
+                                   y_true: y_batch_train}
+                feed_dict_valid = {x: x_batch_valid,
+                                   y_true: y_batch_valid}
+                session.run(self.optimizer, feed_dict=feed_dict_train)
+                if step % int(self.data_process.get_training_data_size()/batch_size) == 0: 
+                    val_loss = session.run(self.cost, feed_dict=feed_dict_valid)
+                    epoch = int(step / int(self.data_process.get_training_data_size()/batch_size))
+            
+                    show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss, session)
+                    saver.save(session, self.model_path + 'trained_model', global_step=step) 
+                    total_num_iter = step
+
+            with open(total_iter_file, 'w') as total_iter_file:
+                total_iter_file.write(total_num_iter)
